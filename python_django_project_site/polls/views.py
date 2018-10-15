73a84bcc59db0a1d8feef6235d68da114d2ed395
Alan Malbos <alanmalbos@gmail.com>
2018-10-15 20:42:23 -0300

    Create the urls to detail, result and vote

--- python_django_project_site/polls/views.py
+++ python_django_project_site/polls/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 # define a index page
 def index(request):
     return HttpResponse('This is the index page of this application!')
+
+def detail(request, question_id):
+    return HttpResponse('This is a question detail view of the question: %s' % question_id)
+
+def results(request, question_id):
+    return HttpResponse('These are a question results view of the question: %s' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Vote on question: %s' % question_id)
