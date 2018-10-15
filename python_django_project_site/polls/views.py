93a2cd9383c7621ffcecf1630eaa0632ff5748a6
Alan Malbos <alanmalbos@gmail.com>
2018-10-15 20:58:51 -0300

    Add the acess to the database from the pages

--- python_django_project_site/polls/views.py
+++ python_django_project_site/polls/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+
 #import a HttpResponse to return the index page
 from django.http import HttpResponse
+
+#import models to work with the database
+from .models import Question
+
 # Create your views here.
 
 # define a index page
 def index(request):
-    return HttpResponse('This is the index page of this application!')
+
+    # get the last five questions created
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+
+    # create a string with de questions text
+    output = ', '.join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse('This is a question detail view of the question: %s' % question_id)
