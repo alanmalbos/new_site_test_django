99e3823b49e8ddafca87689d347397b0584df4b1
Alan Malbos <alanmalbos@gmail.com>
2018-10-17 20:17:21 -0300

    Add a logic for vote page

--- python_django_project_site/polls/views.py
+++ python_django_project_site/polls/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 
-#import a loader to load the templates
+# import a loader to load the templates
 from django.template import loader
 
-#import models to work with the database
+# import a reverse
+from django.urls import reverse
+
+# import models to work with the database
 from .models import Question
 
 # Create your views here.
@@ -34,7 +37,17 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    return HttpResponse('These are a question results view of the question: %s' % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', { 'question' : question })
 
 def vote(request, question_id):
-    return HttpResponse('Vote on question: %s' % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        select_choice = question.choice_set.get(pk = request.POST['choice'])
+    except:
+        return render(request, 'polls/index.html', { 'question' : question, 'error_message' : 'Please select a choice'})
+    else:
+        select_choice.number_of_votes += 1
+        select_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id, )))
